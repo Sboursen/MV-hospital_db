ffe4770a85a4d976364833cb447b3a5b8252fe4a
BonkeG <madixmicro@gmail.com>
2022-05-03 12:32:02 +0200

    Add invoices schema

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -15,3 +15,14 @@ CREATE TABLE
     PRIMARY KEY (id),
     CONSTRAINT patient_fkey FOREIGN KEY (patient_id) REFERENCES patients(id)
   );
+
+CREATE TABLE 
+  invoices(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT,
+    PRIMARY KEY (id),
+    CONSTRAINT medical_history_fkey FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
+  );
